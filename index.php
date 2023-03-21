b1e7ede0677bab0d72327fe0a2d36012a6acf636
AndreaLavezzi <andrea.lavezzi@iisviolamarchesini.edu.it>
2023-03-21 19:41:50 +0100

    la pagina di login restituisce lo user id

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
 <?php
-    session_start();
-    $page = null;
+session_start();
+$page = null;
 
-    if(isset($_GET['page'])){
-        $page = $_GET['page'];
-    }else{
-        $page = 0;
-    }
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+} else {
+    $page = 0;
+}
 ?>
 
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
     <title>BoilerPlate Registro Universitario</title>
 </head>
+
 <body class="d-flex flex-column h-100">
+    <script src="https://code.jquery.com/jquery-3.6.4.js"
+        integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
     <?php
-        require_once("header.php");
-        require_once("main.php");
-        require_once("footer.php");
+    require_once("header.php");
+    require_once("main.php");
+    require_once("footer.php");
     ?>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 
 
 <main class="form-signin w-100 m-auto text-center">
-<link rel="stylesheet" href="styles/login.css">
-  <form>
+  <link rel="stylesheet" href="styles/login.css">
+  <form id="login-form">
     <img class="mb-4" src="images/logo.png" alt="" width="72" height="57">
     <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
@@ -17,7 +17,41 @@
       <label for="floatingPassword">Password</label>
     </div>
 
-    
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+
+    <button class="w-100 btn btn-lg btn-primary" id="submit">Sign in</button>
   </form>
+
+  <script>
+    $("#login-form").submit(function (e) {
+      e.preventDefault();
+      email = $("#floatingInput").val();
+      password = $("#floatingPassword").val();
+
+      console.log(email, password);
+
+      if (email != "" && password != "") {
+        login(email, password);
+        console.log("succ");
+      } else {
+        console.log("no token");
+      }
+    });
+
+    function login(email, password) {
+      $.ajax({
+        type: "POST",
+        url: "/medicina/api/API/user/login.php",
+        data: JSON.stringify({
+          "email": email,
+          "password": password
+        }),
+        success: function (result) {
+          console.log(result.userID);
+        },
+        error: function (xhr, textStatus, errorThrown) {
+          alert(errorThrown);
+        }
+      });
+    }
+  </script>
 </main>
